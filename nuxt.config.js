51e5045e4d70402f858f6adb4cb748421c8f73c7
Marc ABOU CHACRA <marc.abouchacra@gmail.com>
2020-05-24 00:08:44 +0200

    (feat) Add sitemap

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,7 +12,8 @@ export default {
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
     ],
     link: [
-      { rel: 'stylesheet', type: 'text/css', href: 'https://fonts.googleapis.com/css?family=Lato:400,700'}
+      { rel: 'stylesheet', type: 'text/css', href: 'https://fonts.googleapis.com/css?family=Lato:400,700'},
+      { rel: 'sitemap', type: 'application/xml', href: '/sitemap.xml'}
     ]
   },
   /*
@@ -44,9 +45,23 @@ export default {
     // Doc: https://bootstrap-vue.js.org
     'bootstrap-vue/nuxt',
     '@nuxt/http',
+    '@nuxtjs/sitemap',
   ],
   bootstrapVue: {
     icons: true // Install the IconsPlugin (in addition to BootStrapVue plugin
+  },
+  sitemap: {
+    hostname: 'https://latelierdhelena.fr',
+    gzip: true,
+    defaults: {
+      changefreq: 'daily',
+      priority: 1,
+      lastmod: new Date()
+    },
+    exclude: [
+      '/secret',
+      '/admin/**'
+    ]
   },
   /*
   ** Build configuration
