162a7c92f7a74fd644ed6ceea7f21adaeb858b8e
Marc ABOU CHACRA <marc.abouchacra@gmail.com>
2020-06-02 00:09:45 +0200

    (fix) Broken instagram links

--- components/Caroussel.vue
+++ components/Caroussel.vue
@@ -2,58 +2,58 @@
   <b-container>
     <b-row class="pb-3">
       <b-col>
-        <b-img fluid src="https://scontent-cdg2-1.cdninstagram.com/v/t51.2885-15/e35/c257.0.566.566a/95302088_281427186355135_4960045537546119357_n.jpg?_nc_ht=scontent-cdg2-1.cdninstagram.com&_nc_cat=107&_nc_ohc=-IaGdWjLL90AX_sKzRZ&oh=97f3c905f84921621570a0e29a1a5221&oe=5ED2A7B4"></b-img>
+        <b-img fluid src="~static/img/accueil/plateau-mignardises.jpg" alt="Plateau de mignardises"></b-img>
       </b-col>
 
       <b-col> 
-        <b-img fluid src="https://scontent-cdg2-1.cdninstagram.com/v/t51.2885-15/sh0.08/e35/s640x640/94695713_838986646610522_7227632834347583630_n.jpg?_nc_ht=scontent-cdg2-1.cdninstagram.com&_nc_cat=102&_nc_ohc=Wk6bSFF1wWUAX8iplrZ&oh=658831336c82563e3584097d3bbc965f&oe=5ED4527A"></b-img>
+        <b-img fluid src="~static/img/tarte-citron-meringue.webp" alt="Tarte au citron meringué"></b-img>
       </b-col>
       
       <b-col> 
-        <b-img fluid src="https://scontent-cdg2-1.cdninstagram.com/v/t51.2885-15/sh0.08/e35/s640x640/95316991_236965977407635_8514253943348514700_n.jpg?_nc_ht=scontent-cdg2-1.cdninstagram.com&_nc_cat=107&_nc_ohc=zH8pq0Qc3BkAX-_Nl8s&oh=23a4b9843b44ca2528cdca015d0ee116&oe=5ED43A8A"></b-img>
+        <b-img fluid src="~static/img/accueil/entremets-framboise.jpg" alt="Entremets framboise"></b-img>
       </b-col>
     </b-row>
 
     <b-row class="pb-3">
       <b-col> 
-        <b-img fluid src="https://scontent-cdg2-1.cdninstagram.com/v/t51.2885-15/e35/c220.0.639.639a/94723116_133576834948424_815455938211021980_n.jpg?_nc_ht=scontent-cdg2-1.cdninstagram.com&_nc_cat=108&_nc_ohc=alVchJfM4xYAX8hnksW&oh=3125327b6e52ffa35bef8e8a3641124c&oe=5ED4FEC5"></b-img>
+        <b-img fluid src="~static/img/accueil/number-cake-fruits-rouges.jpg" alt="Number cake au fruits rouges"></b-img>
       </b-col>
       
       <b-col> 
-        <b-img fluid src="https://scontent-cdg2-1.cdninstagram.com/v/t51.2885-15/sh0.08/e35/c0.94.1080.1080a/s640x640/95437876_1340459992818308_7982940766613796588_n.jpg?_nc_ht=scontent-cdg2-1.cdninstagram.com&_nc_cat=108&_nc_ohc=f6RiVM8s8GMAX_dz8rV&oh=93cd776d2fd629bb6416f860fa68e9fe&oe=5ED3C8FF"></b-img>
+        <b-img fluid src="~static/img/accueil/fraisier.jpg" alt="Fraisier"></b-img>
       </b-col>
       
       <b-col> 
-        <b-img fluid src="https://scontent-cdt1-1.cdninstagram.com/v/t51.2885-15/sh0.08/e35/c65.0.950.950a/s640x640/94749819_228298648402649_3395354828658220174_n.jpg?_nc_ht=scontent-cdt1-1.cdninstagram.com&_nc_cat=105&_nc_ohc=h4VS8GdkSMUAX_9dVCu&oh=52ea21f283dab2c5a4cb7aea0efc5c56&oe=5ED1E7F3"></b-img>
+        <b-img fluid src="~static/img/tarte-fruit-saison.webp" alt="Tarte aux fruits de saison"></b-img>
       </b-col>
     </b-row>
 
     <b-row class="pb-3">
       <b-col> 
-        <b-img fluid src="https://scontent-cdg2-1.cdninstagram.com/v/t51.2885-15/sh0.08/e35/c0.63.900.900a/s640x640/95260779_863084554103552_2643936613351899857_n.jpg?_nc_ht=scontent-cdg2-1.cdninstagram.com&_nc_cat=108&_nc_ohc=kSQW606t-SAAX__eQDc&oh=f55824987714924288efac42519c04d8&oe=5ED6FFA3"></b-img>
+        <b-img fluid src="~static/img/accueil/gateau-licorne-chocolat.jpg" alt="Gateau au chocolat, décor licorne"></b-img>
         <!-- <b-img fluid src="https://scontent-cdg2-1.cdninstagram.com/v/t51.2885-15/e35/c243.0.594.594a/95029706_273866843612109_3903608684504309857_n.jpg?_nc_ht=scontent-cdg2-1.cdninstagram.com&_nc_cat=109&_nc_ohc=o33LMTqog8AAX-ooedn&oh=b4a5d722c5c289ef07ab2012ac530494&oe=5ED431D3"></b-img> -->
       </b-col>
       
       <b-col> 
-        <b-img fluid src="https://scontent-cdg2-1.cdninstagram.com/v/t51.2885-15/sh0.08/e35/s640x640/94890211_234041484490616_8373306569317993977_n.jpg?_nc_ht=scontent-cdg2-1.cdninstagram.com&_nc_cat=108&_nc_ohc=A1hjjX9ENDoAX8bOdmR&oh=9bb90d6f6e2bbf84d14784296469a243&oe=5ED336B3"></b-img>
+        <b-img fluid src="~static/img/entremet-citron.webp" alt="Entremets tout citron"></b-img>
       </b-col>
       
       <b-col> 
-        <b-img fluid src="https://scontent-cdt1-1.cdninstagram.com/v/t51.2885-15/sh0.08/e35/s640x640/94917960_333736234276325_2635394742474092361_n.jpg?_nc_ht=scontent-cdt1-1.cdninstagram.com&_nc_cat=105&_nc_ohc=-Zib1zv1lnEAX9Z1QE8&oh=be862cf9ff5f00c01be9a21b4d81ea85&oe=5ED48D1D"></b-img>
+        <b-img fluid src="~static/img/accueil/macarons-chocolat-passion.jpg" alt="Macarons chocolat et passion"></b-img>
       </b-col>
     </b-row>
 
     <b-row class="pb-3">
       <b-col> 
-        <b-img fluid src="https://scontent-cdg2-1.cdninstagram.com/v/t51.2885-15/sh0.08/e35/s640x640/94845435_251786182545045_4212925178911368253_n.jpg?_nc_ht=scontent-cdg2-1.cdninstagram.com&_nc_cat=109&_nc_ohc=n6kewn5sSWIAX9g5kUp&oh=fa7536f78121cc94c4c5e74e8afb142a&oe=5ED8C8C3"></b-img>
+        <b-img fluid src="~static/img/accueil/tarte-fruits-rouges.jpg" alt="Tarte aux fruits rouges"></b-img>
       </b-col>
       
       <b-col> 
-        <b-img fluid src="https://scontent-cdg2-1.cdninstagram.com/v/t51.2885-15/sh0.08/e35/c39.0.1002.1002a/s640x640/94904749_152892806224786_4613297673644030244_n.jpg?_nc_ht=scontent-cdg2-1.cdninstagram.com&_nc_cat=110&_nc_ohc=kMHhjbVS79oAX_AcRLp&oh=0ab99f35cac1c67bb4bea57b7070239b&oe=5ED47705"></b-img>
+        <b-img fluid src="~static/img/accueil/gateaux-anniversaire-chocolat.jpg" alt="Gateau d'anniversaire au chocolat. Thème super héros"></b-img>
       </b-col>
       
       <b-col> 
-        <b-img fluid src="https://scontent-cdg2-1.cdninstagram.com/v/t51.2885-15/sh0.08/e35/c0.135.1080.1080a/s640x640/95330052_3062065987214632_44878417037219293_n.jpg?_nc_ht=scontent-cdg2-1.cdninstagram.com&_nc_cat=104&_nc_ohc=TbNn3wEcg-8AX97Osgw&oh=9d899ae05f0fa2d1a5add07f4bea32a9&oe=5ED29E64"></b-img>
+        <b-img fluid src="~static/img/nude-cake.webp" alt="Nude cake à la fraise"></b-img>
         <!-- <b-img fluid src="https://scontent-cdg2-1.cdninstagram.com/v/t51.2885-15/e35/c236.0.608.608a/94708811_241585563714286_4184395814361506019_n.jpg?_nc_ht=scontent-cdg2-1.cdninstagram.com&_nc_cat=110&_nc_ohc=tUeBTw70TPUAX-kFRHv&oh=81f467caba24cc651b9ff0fbf40addcd&oe=5ED51263"></b-img> -->
       </b-col>
     </b-row>
