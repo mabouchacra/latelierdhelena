6886baf82335008df35b34463881a3ac85439c5a
Marc ABOU CHACRA <marc.abouchacra@gmail.com>
2021-12-10 09:23:53 +0100

    update contact

--- pages/contact.vue
+++ pages/contact.vue
@@ -10,8 +10,8 @@
           </b-row>
           <b-row>
             <b-col class="text-justify" md="6">
-              <p>Je m'appelle Héléna, j’ai 33 ans, je suis mariée et maman de deux merveilleux petits garçons.<br/>
-J’ai toujours aimé cuisiner et faire des gâteaux, mais il a fallu attendre mes 32 ans pour me rendre compte que c’était vraiment quelque chose qui me passionnait et que je pouvais le partager au plus grand nombre. En effet, il y a encore quelques mois, j’exerçais la profession de responsable qualité d’un logiciel informatique dans le biomédical.</p>
+              <p>Je m'appelle Héléna, je suis mariée et maman de trois merveilleux enfants.<br/>
+J’ai toujours aimé cuisiner et faire des gâteaux, mais il a fallu attendre mes 32 ans pour me rendre compte que c’était vraiment quelque chose qui me passionnait et que je pouvais le partager au plus grand nombre. En effet, il n’y a pas si longtemps, j’exerçais la profession de responsable qualité d’un logiciel informatique dans le biomédical.</p>
               <p class="text-justify">
                 J’ai donc passé un CAP pâtissier en  alternance sur 1 an au lycée hôtelier de Talence, en multipliant les expériences professionnelles : dans une  pâtisserie A la folie à Pessac, dans l'équipe de Franck Labasse, ... En parallèle, je lance ma page facebook <a href="https://www.facebook.com/atelierdhelena" target="_blank">L'atelier d'Héléna</a>, afin de partager mes réalisations au plus grand nombre. Attachée à la technique et aux bons produits, j’ai à cœur de proposer des recettes de saison et légères en sucre mais sans jamais oublier la gourmandise ! Me voilà aujourd’hui Ingénieur - Pâtissière - Entrepreneuse. Que l'aventure commence !
               </p>
